66e8ccf64cfc409fce672bd1c097a86479b0dc9c
Denis Sergeev <denis-sergeev-85@inbox.ru>
2022-06-08 22:39:55 +0300

    создано описание

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function Categories({ value, onChangeCategory }) {
-  const categories = ["Все", "Мясные", "Вегетарианская", "Гриль", "Острые", ""];
+  const categories = [
+    "Все",
+    "Мясные",
+    "Вегетарианская",
+    "Гриль",
+    "Острые",
+    "Закрытыеs",
+  ];
 
   return (
     <div className="categories">
