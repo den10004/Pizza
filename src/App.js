26fff685ea24955e53671521d8e52b086f4405f9
Denis Sergeev <denis-sergeev-85@inbox.ru>
2022-06-08 10:51:01 +0300

    создана корзина

--- src/App.js
+++ src/App.js
@@ -9,16 +9,14 @@ import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <div className="wrapper">
-        <Header />
-        <div className="content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </div>
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </div>
     </div>
   );
