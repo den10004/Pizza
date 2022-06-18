91ca24f69a4df586d5015dfbc3da4d3ad34ba94a
Denis Sergeev <denis-sergeev-85@inbox.ru>
2022-06-18 16:33:54 +0300

    12 урок

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import { Routes, Route } from "react-router-dom";
 import { NotFoundBlock } from "./components/NotFoundBlock/index";
 import { Cart } from "./pages/Cart";
 
+import { store } from "./redux/store";
+
 export const SearchContext = React.createContext();
 
 function App() {
