9430704ae30d692201ed85a8833658515f23072e
Denis Sergeev <denis-sergeev-85@inbox.ru>
2022-06-29 17:04:38 +0300

    исправлена вёрстка

--- src/Pages/FullPizza.tsx
+++ src/Pages/FullPizza.tsx
@@ -38,11 +38,13 @@ const FullPizza: React.FC = () => {
       <img src={pizza.imageUrl} />
       <h2>{pizza.title}</h2>
       <h4>{pizza.price} ₽</h4>
-      <Link to="/">
-        <button className="button button--outline button--add">
-          <span>Назад</span>
-        </button>
-      </Link>
+      <div style={{ marginTop: "20px" }}>
+        <Link to="/">
+          <button className="button button--outline button--add">
+            <span>Назад</span>
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
